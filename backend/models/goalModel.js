ef57f9304cf3c58cc5f03cf7e9910f0c3e874092
Julyandera <rayijulyandera03@gmail.com>
2023-01-06 19:29:22 +0700

    Add UI

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,12 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const goalSchema = mongoose.Schema({
-  text: {
-    type: String,
-    requires: [true, 'Please add text!']
+const goalSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    text: {
+      type: String,
+      requires: [true, 'Please add text!'],
+    },
+  },
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-})
+)
 
 module.exports = mongoose.model('Goal', goalSchema)
